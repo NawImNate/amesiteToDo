57bfe18659caf207d66f05b312d6ab9c1d7963f3
NawImNate <jnhatch3@gmail.com>
2022-05-13 13:39:48 -0400

    creating routes testing post request on postman

--- server/index.js
+++ server/index.js
@@ -7,6 +7,26 @@ const Pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+//Routes//
+
+//Create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
 app.listen(5000, () => {
   console.log("working, on 5000.");
 });
