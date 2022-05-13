da2f72bc2f58bd513bf7070db4aef646472d74dc
NawImNate <jnhatch3@gmail.com>
2022-05-13 12:37:16 -0400

    added placeholder to input

--- client/src/Form.js
+++ client/src/Form.js
@@ -4,7 +4,12 @@ const Form = () => {
   return (
     <div>
       <form>
-        <input className="input-todo" type="text" onChange="{}" />
+        <input
+          className="input-todo"
+          type="text"
+          placeholder="Create a task"
+          onChange="{}"
+        />
         <button className="submit-todo" type="submit" onClick="{}">
           +
         </button>
