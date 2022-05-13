08ff97bba4c51440bd04fe045074afcb91522ddd
NawImNate <jnhatch3@gmail.com>
2022-05-13 15:44:51 -0400

    building form component

--- client/src/Form.js
+++ client/src/Form.js
@@ -3,16 +3,34 @@ import React from "react";
 const Form = () => {
   return (
     <div>
-      <form>
-        <input
-          className="input-todo"
-          type="text"
-          placeholder="Create a task"
-          onChange="{}"
-        />
-        <button className="submit-todo" type="submit" onClick="{}">
-          +
-        </button>
+      <form className="todo-container">
+        <div className="todo-container-card">
+          <p>Title</p>
+          <input
+            className="input-todo-title"
+            type="text"
+            placeholder="Create a task"
+            onChange="{}"
+          />
+        </div>
+        <div className="todo-container-card">
+          <p>Description</p>
+          <input
+            className="input-todo-description"
+            type="text"
+            placeholder="Create a description"
+            onChange="{}"
+          />
+        </div>
+        <div className="todo-container-card">
+          <p>Due Date</p>
+          <input className="input-todo-duedate" type="date" />
+        </div>
+        <div className="todo-container-card2">
+          <button className="submit-todo" type="submit" onClick="{}">
+            +
+          </button>
+        </div>
       </form>
     </div>
   );
