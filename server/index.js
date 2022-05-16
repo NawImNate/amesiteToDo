4beedc20faca55ace18012da10740aef73968d94
NawImNate <jnhatch3@gmail.com>
2022-05-16 13:55:50 -0400

    client post request insert statement test

--- server/index.js
+++ server/index.js
@@ -1,25 +1,48 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const Pool = require("./db");
+// const pool = require("./db");
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 
+const { Pool, Client } = require("pg");
+
+//Instance pool, specify credentials
+const credentials = {
+  user: "postgres",
+  password: "Database0894",
+  host: "localhost",
+  port: 5432,
+  database: "amesiteToDo",
+};
+
+const pool = new Pool(credentials);
+
 //Routes//
 
 //Create a todo
-
 app.post("/todos", async (req, res) => {
-  try {
-    console.log(req.body);
-  } catch (err) {
-    console.log(err.message);
-  }
+  // const { TodoItem } = req.body;
+
+  const result = await pool.query(
+    `INSERT INTO "todo_item" ("title", "description", "due_date", "tag_id", "list_id") VALUES ($1, $2, $3, $4, $5) RETURNING todo_id`,
+    [req.body.title, req.body.description, req.body.dueDate, 1, 1]
+  );
+  console.log(result.rows[0].todo_id);
+  res.json(result.rows[0].todo_id);
 });
 
 //get all todos
+
+app.get("/todos", async (req,res) => {
+  try {
+    const 
+  } catch (error) {
+    
+  }
+})
 
 //get a todo
 
