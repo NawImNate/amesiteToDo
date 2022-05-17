4d827eed3174cf53961cecc30b18a29b582b61da
NawImNate <jnhatch3@gmail.com>
2022-05-17 14:19:32 -0400

    login & authentication integration, post request

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,64 @@ import Form from "./Form";
 import TodoList from "./TodoList";
 
 function App() {
+  const [Authenticated, setAuthenticated] = useState(false);
+  const [login, setLogin] = useState(null);
+
+  // check if user exists, create if user doesn't exist
+  const checkUser = async () => {
+    try {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ login }),
+      };
+      const response = await fetch(
+        "http://localhost:5000/users",
+        requestOptions
+      );
+
+      const data = await response.json();
+
+      console.log(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  console.log(login);
   return (
     <div className="App">
-      <header>
-        <h1>Todo List</h1>
-        <Form />
-        <TodoList />
-      </header>
+      {Authenticated ? (
+        <div>
+          <h1>Todo List</h1>
+          <Form />
+          <TodoList />
+        </div>
+      ) : (
+        <div>
+          <div>
+            <input
+              placeholder="Enter Email"
+              type="text"
+              onChange={(e) => setLogin({ ...login, email: e.target.value })}
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Enter Password"
+              type="text"
+              onChange={(e) => setLogin({ ...login, password: e.target.value })}
+            />
+          </div>
+        </div>
+      )}
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          checkUser();
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
