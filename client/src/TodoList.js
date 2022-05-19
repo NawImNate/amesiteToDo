67ad754d8d370dcca187fbe282742b90db8c78c5
NawImNate <jnhatch3@gmail.com>
2022-05-19 12:23:52 -0400

    edit & delete btn added, reload list after delete

--- client/src/TodoList.js
+++ client/src/TodoList.js
@@ -1,7 +1,26 @@
 import { React, useEffect, useState } from "react";
 
-const TodoList = ({ ToDoList }) => {
-  // async function deleteTodo(postID){}
+const TodoList = ({ ToDoList, triggerReload, passPropsForEdit }) => {
+  async function deleteTodo(todo_id) {
+    console.log(todo_id);
+    try {
+      const requestOptions = {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      };
+      const response = await fetch(
+        `http://localhost:5000/status/${todo_id}`,
+        requestOptions
+      );
+      console.log(response);
+      const data = await response.json();
+      if (data != null) {
+        triggerReload();
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 
   return (
     <div className="todoList-container">
@@ -10,6 +29,22 @@ const TodoList = ({ ToDoList }) => {
           <div>
             {ToDoList.map((post) => (
               <div>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    deleteTodo(post.todo_id);
+                  }}
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    passPropsForEdit(post);
+                  }}
+                >
+                  Edit
+                </button>
                 <li key={post.id}>{post.description}</li>
                 <span>{post.title}</span>
               </div>
